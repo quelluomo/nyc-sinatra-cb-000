5261fbc4f59fcf844a8684979c9b69c6aed44f36
Matteo Muscas <thatman.j@gmail.com>
2019-04-19 09:07:05 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,13 +1,13 @@
 class LandmarksController < ApplicationController
   # add controller methods
 
-  get '/landmarks/new' do
-    erb :'/landmarks/new'
-  end
-
   get '/landmarks' do
     @landmarks = Landmark.all
     erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
   end
 
   get '/landmarks/:id' do
