7ba3dc7671fedf4d20e5197de3321145fa8559f8
Matteo Muscas <thatman.j@gmail.com>
2019-04-19 08:25:24 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,52 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/show'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params["figure"])
+
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(:name => params["landmark"]["name"])
+    end
+
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(:name => params["title"]["name"])
+    end
+
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params["figure"])
+
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(:name => params["landmark"]["name"])
+    end
+
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(:name => params["title"]["name"])
+    end
+
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,24 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(:name => params["landmark"]["name"], :year_completed => params["landmark"]["year_completed"])
+
+    @landmark.save
+    redirect to "/landmarks/show"
+  end
 end
